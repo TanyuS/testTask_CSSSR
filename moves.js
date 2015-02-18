6401b9f62e619016b9a7796da5b204e9363eaaf7
TanyuS <tanyusokolova@gmail.com>
2015-02-18 23:21:50 +0600

    page

---
+++ moves.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+
+	$('#lvl0').click(function() {
+		$('#scroller').css('left', '0px');
+	});
+	$('#lvl1').click(function() {
+		$('#scroller').css('left', '148px');
+	});
+	$('#lvl2').click(function() {
+		$('#scroller').css('left', '373px');
+	});
+	$('#lvl3').click(function() {
+		$('#scroller').css('left', '755px');
+	});
+	
+	
+});
